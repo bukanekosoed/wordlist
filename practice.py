b46d8852864ae1819132d6cc49615d3a96de2746
bukanekosoed <140147721+bukanekosoed@users.noreply.github.com>
2023-11-10 19:53:44 +0700

    Add files via upload
    First

---
+++ practice.py
@@ -0,0 +1,9 @@
+import requests
+
+api_key = 'f45737fe-676b-4025-bbd9-ca2073c78cc1'
+word = 'voluminous'
+root_url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json'
+final_url = f'{root_url}/{word}?key={api_key}'
+r = requests.get(final_url)
+result = r.json()
+print(result)
